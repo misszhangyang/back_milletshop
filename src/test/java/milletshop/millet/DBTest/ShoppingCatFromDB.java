868a96fb4c004b3c97719cf753d2ef6c4518630c
zhangyang <690196784@qq.com>
2019-07-25 12:41:52 +0800

    新增修改配置文件以及端口号等和服务器保持一致

--- src/test/java/milletshop/millet/DBTest/ShoppingCatFromDB.java
+++ src/test/java/milletshop/millet/DBTest/ShoppingCatFromDB.java
@@ -3,9 +3,12 @@ package milletshop.millet.DBTest;
 import milletshop.millet.mapper.ShoppingCarMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Scanner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,11 +19,26 @@ public class ShoppingCatFromDB {
 
     @Test
     public void test(){
-        if(shoppingCarMapper == null)
-            System.out.println("the springcarMapper is null");
-        else{
-            int x = shoppingCarMapper.delShoppingPhoneById(3);
-            System.out.println(x);
-        }
+
+        Scanner scanner = new Scanner(System.in);
+        char c = ' ';
+        System.out.println("请输入一个数：");
+        c = scanner.next().charAt(0);
+        System.out.println("输入的数是："+c);
+        System.out.println("qeqweqw");
+
+        String str = "1212大旗网大旗网的";
+//        if(str.length() > 10){
+//            System.out.println(str.length());
+//            System.out.println(str.substring(0,10));
+//        }
+
+
+//        if(shoppingCarMapper == null)
+//            System.out.println("the springcarMapper is null");
+//        else{
+//            int x = shoppingCarMapper.delShoppingPhoneById(3);
+//            System.out.println(x);
+//        }
     }
 }
